112baa04681e3a12b40ce7cddb7c055a3d9eff5c
Javid Mammadzada <mammadzadacavid0@gmail.com>
2023-12-04 10:23:00 +0400

    new file for functionality of detailed product page

---
+++ product.js
@@ -0,0 +1,81 @@
+
+function getQueryParam() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('id');
+}
+
+async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProductDetailed(data);
+}
+
+function displayProductDetailed(productData) {
+    console.log(productData);
+    const productDetailedContainer = document.querySelector('.product-detailed');
+
+    const productMainImg = document.createElement('img');
+    const productTitle = document.createElement('div');
+    const productCategory = document.createElement('div');
+    const productPrice = document.createElement('div');
+    const productDiscount = document.createElement('div');
+    const productStock = document.createElement('div');
+    const productDescription = document.createElement('div');
+    const productBrand = document.createElement('div');
+    const productBuy = document.createElement('button');
+    const productInfoContainer = document.createElement('div');
+    const productGeneralContainer = document.createElement('div');
+    const productImgsContainer = document.createElement('div');
+
+    productMainImg.src = productData.thumbnail
+    productMainImg.alt = "product_detailed_image";
+
+    productTitle.innerText = productData.title;
+    productTitle.classList.add('product-detailed-title');
+
+    productCategory.innerText = productData.category.charAt(0).toUpperCase() + productData.category.slice(1);
+    productCategory.classList.add('product-detailed-category');
+
+    productPrice.innerText = "$" + productData.price;
+    productPrice.classList.add('product-detailed-price');
+
+    productDiscount.innerText = "- " + productData.discountPercentage.toFixed() + "%";
+    productDiscount.classList.add('product-detailed-discount');
+
+    productStock.innerText = productData.stock;
+    productDiscount.classList.add('product-detailed-stock');
+
+    productDescription.innerText = productData.description;
+    productDescription.classList.add('product-detailed-description');
+
+    productBuy.innerText = 'Add to Cart';
+    productBuy.classList.add('product-detailed-buy');
+
+    productBrand.innerText = productData.brand;
+    productBrand.classList.add('product-detailed-brand');
+
+    productInfoContainer.appendChild(productBrand);
+    productInfoContainer.appendChild(productTitle);
+    productInfoContainer.appendChild(productCategory);
+    productInfoContainer.appendChild(productDescription);
+    productInfoContainer.appendChild(productPrice);
+    productInfoContainer.appendChild(productStock);
+    productInfoContainer.appendChild(productBuy);
+
+    productGeneralContainer.appendChild(productMainImg);
+    productGeneralContainer.appendChild(productInfoContainer);
+    productGeneralContainer.classList.add('product-detailed-general')
+
+    productData.images.forEach(image => {
+        const productImg = document.createElement('img');
+        productImg.src = image;
+        productImg.alt = "detailed_image";
+        productImgsContainer.appendChild(productImg);
+    });
+
+    productDetailedContainer.appendChild(productGeneralContainer);
+    productDetailedContainer.appendChild(productImgsContainer);
+}
+
+const productId = getQueryParam();
+getData(`https://dummyjson.com/products/${productId}`);
